f4fd618fdc38daca8654490719cc8d06a2845460
mauricio gonzales <[mauri9825@gmail.com]>
2025-09-15 23:25:14 -0300

    Ejemplo

---
+++ TarjetaSube/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TarjetaSube
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TarjetaSube/Tarjeta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TarjetaSube
+{
+    public class Tarjeta
+    {
+        private int saldo;
+        public Tarjeta(int saldo = 0)
+        {
+            this.saldo = saldo;
+        }
+
+        public int Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+        
+        public void Cargar(int importe)
+        {
+            saldo += importe;
+        }
+        public void Pagar()
+        {
+            saldo -= 50;
+        }
+    }
+}
