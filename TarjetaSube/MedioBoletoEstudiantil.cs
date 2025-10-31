5d87c2fb021c606affc72239b37e66b81fdf2ef9
yucovsky <franciscoyucovsky@gmail.com>
2025-10-30 21:58:22 -0300

    implementar limitaciones para medio boleto

--- TarjetaSube/MedioBoletoEstudiantil.cs
+++ TarjetaSube/MedioBoletoEstudiantil.cs
@@ -1,12 +1,18 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TarjetaSube
 {
     public class MedioBoletoEstudiantil : Tarjeta
     {
+        private List<DateTime> viajesHoy;
+        private const int MINUTOS_ENTRE_VIAJES = 5;
+        private const int MAX_VIAJES_POR_DIA = 2;
+
         public MedioBoletoEstudiantil(int saldo = 0) : base(saldo)
         {
+            viajesHoy = new List<DateTime>();
         }
 
         public override string TipoTarjeta
@@ -16,7 +22,54 @@ namespace TarjetaSube
 
         public override int CalcularMontoPasaje(int tarifaBase)
         {
-            return tarifaBase / 2;
+            LimpiarViajesAntiguos(DateTime.Now);
+            return viajesHoy.Count < MAX_VIAJES_POR_DIA ? tarifaBase / 2 : tarifaBase;
+        }
+
+        public void RegistrarViaje(DateTime fechaHoraViaje)
+        {
+            LimpiarViajesAntiguos(fechaHoraViaje);
+
+            if (viajesHoy.Count >= MAX_VIAJES_POR_DIA)
+            {
+                throw new InvalidOperationException($"Límite de {MAX_VIAJES_POR_DIA} viajes por día alcanzado con medio boleto");
+            }
+
+            if (viajesHoy.Count > 0)
+            {
+                DateTime ultimoViaje = viajesHoy.Last();
+                TimeSpan tiempoDesdeUltimoViaje = fechaHoraViaje - ultimoViaje;
+                if (tiempoDesdeUltimoViaje.TotalMinutes < MINUTOS_ENTRE_VIAJES)
+                {
+                    throw new InvalidOperationException($"Deben pasar al menos {MINUTOS_ENTRE_VIAJES} minutos entre viajes con medio boleto");
+                }
+            }
+
+            viajesHoy.Add(fechaHoraViaje);
+        }
+
+        public int ViajesHoy
+        {
+            get 
+            { 
+                LimpiarViajesAntiguos(DateTime.Now);
+                return viajesHoy.Count; 
+            }
+        }
+
+        public DateTime? UltimoViaje
+        {
+            get 
+            { 
+                LimpiarViajesAntiguos(DateTime.Now);
+                return viajesHoy.Count > 0 ? viajesHoy.Last() : (DateTime?)null; 
+            }
+        }
+
+        private void LimpiarViajesAntiguos(DateTime fechaReferencia)
+        {
+            DateTime hoy = fechaReferencia.Date; 
+            viajesHoy.RemoveAll(viaje => viaje.Date < hoy);
         }
 
         public override bool EsFranquiciaGratuita()
---
+++ TarjetaSubeTest/MedioBoletoLimitacionTest.cs
@@ -0,0 +1,121 @@
+using TarjetaSube;
+using NUnit.Framework;
+using System;
+
+namespace TarjetaSubeTest
+{
+    public class MedioBoletoLimitacionTest
+    {
+        private const int TARIFA_BASICA = 1580;
+
+        [Test]
+        public void MedioBoleto_NoPermiteSegundoViajeAntesDe5Minutos()
+        {
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+        
+            DateTime primerViaje = new DateTime(2024, 10, 30, 10, 0, 0);
+            DateTime segundoViaje = new DateTime(2024, 10, 30, 10, 4, 0); 
+        
+            tarjeta.RegistrarViaje(primerViaje);
+        
+            Assert.Throws<InvalidOperationException>(() => tarjeta.RegistrarViaje(segundoViaje));
+        }
+        
+        [Test]
+        public void MedioBoleto_NoPermiteMasDeDosViajesPorDia()
+        {
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+        
+            DateTime primerViaje = new DateTime(2024, 10, 30, 10, 0, 0);
+            DateTime segundoViaje = new DateTime(2024, 10, 30, 11, 0, 0);
+            DateTime tercerViaje = new DateTime(2024, 10, 30, 12, 0, 0);
+        
+            tarjeta.RegistrarViaje(primerViaje);
+        
+            tarjeta.RegistrarViaje(segundoViaje);
+        
+            Assert.Throws<InvalidOperationException>(() => tarjeta.RegistrarViaje(tercerViaje));
+        }
+
+        [Test]
+        public void MedioBoleto_CalcularMontoPasaje_TercerViajeCompleto()
+        {
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+
+            DateTime hoy = DateTime.Today;
+
+            tarjeta.RegistrarViaje(hoy.AddHours(10));
+            tarjeta.RegistrarViaje(hoy.AddHours(11));
+
+            int monto = tarjeta.CalcularMontoPasaje(TARIFA_BASICA);
+            Assert.AreEqual(TARIFA_BASICA, monto);
+        }
+
+        [Test]
+        public void MedioBoleto_ViajesHoy_ContadorCorrecto()
+        {
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+
+            DateTime hoy = DateTime.Today;
+
+            Assert.AreEqual(0, tarjeta.ViajesHoy);
+
+            tarjeta.RegistrarViaje(hoy.AddHours(10));
+            Assert.AreEqual(1, tarjeta.ViajesHoy);
+
+            tarjeta.RegistrarViaje(hoy.AddHours(11));
+            Assert.AreEqual(2, tarjeta.ViajesHoy);
+
+            Assert.Throws<InvalidOperationException>(() => 
+                tarjeta.RegistrarViaje(hoy.AddHours(12)));
+        }
+
+        [Test]
+        public void MedioBoleto_PermiteSegundoViajeDespuesDe5Minutos()
+        {
+            Colectivo colectivo = new Colectivo("132", 1234);
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+
+            DateTime primerViaje = new DateTime(2024, 10, 30, 10, 0, 0);
+            DateTime segundoViaje = new DateTime(2024, 10, 30, 10, 6, 0);
+
+            Boleto boleto1 = colectivo.PagarConEnFecha(tarjeta, primerViaje);
+            Assert.AreEqual(TARIFA_BASICA / 2, boleto1.Monto);
+
+            Boleto boleto2 = colectivo.PagarConEnFecha(tarjeta, segundoViaje);
+            Assert.AreEqual(TARIFA_BASICA / 2, boleto2.Monto);
+        }
+
+        [Test]
+        public void MedioBoleto_ReiniciaContadorAlDiaSiguiente()
+        {
+            Colectivo colectivo = new Colectivo("132", 1234);
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+
+            DateTime dia1Viaje1 = new DateTime(2024, 10, 30, 23, 0, 0);
+            DateTime dia1Viaje2 = new DateTime(2024, 10, 30, 23, 6, 0);
+            DateTime dia2Viaje1 = new DateTime(2024, 10, 31, 1, 0, 0);
+
+            Boleto boleto1 = colectivo.PagarConEnFecha(tarjeta, dia1Viaje1);
+            Boleto boleto2 = colectivo.PagarConEnFecha(tarjeta, dia1Viaje2);
+            Assert.AreEqual(TARIFA_BASICA / 2, boleto1.Monto);
+            Assert.AreEqual(TARIFA_BASICA / 2, boleto2.Monto);
+
+            Boleto boleto3 = colectivo.PagarConEnFecha(tarjeta, dia2Viaje1);
+            Assert.AreEqual(TARIFA_BASICA / 2, boleto3.Monto);
+        }
+
+        [Test]
+        public void MedioBoleto_PagarConBoolean_RespetaLimitacion5Minutos()
+        {
+            Colectivo colectivo = new Colectivo("132", 1234);
+            MedioBoletoEstudiantil tarjeta = new MedioBoletoEstudiantil(5000);
+
+            bool primerViaje = colectivo.PagarConBoolean(tarjeta);
+            Assert.IsTrue(primerViaje);
+
+            bool segundoViaje = colectivo.PagarConBoolean(tarjeta);
+            Assert.IsFalse(segundoViaje);
+        }
+    }
+}
